c17b7c648176a6059dbe864f19ea2c592da40c96
Sean Gregory <seangregory@gmail.com>
2018-04-29 16:04:14 -0400

    move gradient declaration

--- src/js/work.vue
+++ src/js/work.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="getClass" :style="getStyle">
-    <div class="gradient" :style="getGradient"></div>
+    <div class="work__gradient gradient" :style="getGradient"></div>
     <div class="work__details">
       <div class="work__details__header">
         <div class="work__details__header__logo">
@@ -55,10 +55,3 @@ module.exports = {
   }
 }
 </script>
-<style>
-.gradient {
-  width: 100%;
-  height: 100%;
-  position: absolute;
-}
-</style>
